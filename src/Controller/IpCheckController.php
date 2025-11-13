2debdb3bbda3c70d12d3737644047f8b85a338aa
CD10h <simas.briedis@gmail.com>
2025-11-13 15:20:51 +0200

    remove unnecessary argument

--- src/Controller/IpCheckController.php
+++ src/Controller/IpCheckController.php
@@ -18,7 +18,6 @@ class IpCheckController extends AbstractController
 {
 
     public function __construct(
-        private EntityManagerInterface $entityManager,
         private ValidatorInterface $validator,
         private IPInfoChecker $ipInfoChecker,
         private BlacklistService $blacklistService
